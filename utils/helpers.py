621d19524db79fc07f33523b9b5bb03401f3f721
jalel <jalel.zghonda@epfl.ch>
2019-12-10 18:27:01 +0100

    moved helper functions to helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,25 +1,64 @@
-# Helper functions
-import matplotlib.image as mpimg
 import numpy as np
 
+from PIL import Image
+import tensorflow as tf
+import matplotlib.image as mpimg
 
-def load_image(infilename):
-    data = mpimg.imread(infilename)
-    return data
+
+# Write predictions from neural network to a file
+def write_predictions_to_file(predictions, labels, filename):
+    max_labels = np.argmax(labels, 1)
+    max_predictions = np.argmax(predictions, 1)
+    file = open(filename, "w")
+    n = predictions.shape[0]
+    for i in range(0, n):
+        file.write(max_labels(i) + ' ' + max_predictions(i))
+    file.close()
 
 
-def img_float_to_uint8(img):
+# Print predictions from neural network
+def print_predictions(predictions, labels):
+    max_labels = np.argmax(labels, 1)
+    max_predictions = np.argmax(predictions, 1)
+    print(str(max_labels) + ' ' + str(max_predictions))
+
+
+# Assign a label to a patch v
+def value_to_class(v):
+    foreground_threshold = 0.25  # percentage of pixels > 1 required to assign a foreground label to a patch
+    df = np.sum(v)
+    if df > foreground_threshold:  # road
+        return [0, 1]
+    else:  # bgrd
+        return [1, 0]
+
+
+# Convert array of labels to an image
+def label_to_img(imgwidth, imgheight, w, h, labels):
+    array_labels = np.zeros([imgwidth, imgheight])
+    idx = 0
+    for i in range(0, imgheight, h):
+        for j in range(0, imgwidth, w):
+            if labels[idx][0] > 0.5:  # bgrd
+                l = 0
+            else:
+                l = 1
+            array_labels[j:j + w, i:i + h] = l
+            idx = idx + 1
+    return array_labels
+
+
+def img_float_to_uint8(img, pixel_depth):
     rimg = img - np.min(img)
-    rimg = (rimg / np.max(rimg) * 255).round().astype(np.uint8)
+    rimg = (rimg / np.max(rimg) * pixel_depth).round().astype(np.uint8)
     return rimg
 
 
-# Concatenate an image and its groundtruth
 def concatenate_images(img, gt_img):
-    nChannels = len(gt_img.shape)
+    n_channels = len(gt_img.shape)
     w = gt_img.shape[0]
     h = gt_img.shape[1]
-    if nChannels == 3:
+    if n_channels == 3:
         cimg = np.concatenate((img, gt_img), axis=1)
     else:
         gt_img_3c = np.zeros((w, h, 3), dtype=np.uint8)
@@ -32,6 +71,46 @@ def concatenate_images(img, gt_img):
     return cimg
 
 
+def make_img_overlay(img, predicted_img, pixel_depth):
+    w = img.shape[0]
+    h = img.shape[1]
+    color_mask = np.zeros((w, h, 3), dtype=np.uint8)
+    color_mask[:, :, 0] = predicted_img * pixel_depth
+
+    img8 = img_float_to_uint8(img)
+    background = Image.fromarray(img8, 'RGB').convert("RGBA")
+    overlay = Image.fromarray(color_mask, 'RGB').convert("RGBA")
+    new_img = Image.blend(background, overlay, 0.2)
+    return new_img
+
+
+# Make an image summary for 4d tensor image with index idx
+def get_image_summary(img, pixel_depth, idx=0):
+    V = tf.slice(img, (0, 0, 0, idx), (1, -1, -1, 1))
+    img_w = img.get_shape().as_list()[1]
+    img_h = img.get_shape().as_list()[2]
+    min_value = tf.reduce_min(V)
+    V = V - min_value
+    max_value = tf.reduce_max(V)
+    V = V / (max_value * pixel_depth)
+    V = tf.reshape(V, (img_w, img_h, 1))
+    V = tf.transpose(V, (2, 0, 1))
+    V = tf.reshape(V, (-1, img_w, img_h, 1))
+    return V
+
+
+# Make an image summary for 3d tensor image with index idx
+def get_image_summary_3d(img):
+    V = tf.slice(img, (0, 0, 0), (1, -1, -1))
+    img_w = img.get_shape().as_list()[1]
+    img_h = img.get_shape().as_list()[2]
+    V = tf.reshape(V, (img_w, img_h, 1))
+    V = tf.transpose(V, (2, 0, 1))
+    V = tf.reshape(V, (-1, img_w, img_h, 1))
+    return V
+
+
+# Extract patches from a given image
 def img_crop(im, w, h):
     list_patches = []
     imgwidth = im.shape[0]
@@ -45,3 +124,52 @@ def img_crop(im, w, h):
                 im_patch = im[j:j + w, i:i + h, :]
             list_patches.append(im_patch)
     return list_patches
+
+
+def extract_data(filename, num_images, img_patch_size):
+    """Extract the images into a 4D tensor [image index, y, x, channels].
+    Values are rescaled from [0, 255] down to [-0.5, 0.5].
+    """
+    imgs = []
+    for i in range(1, num_images + 1):
+        imageid = "satImage_%.3d" % i
+        image_filename = filename + imageid + ".png"
+        if os.path.isfile(image_filename):
+            print('Loading ' + image_filename)
+            img = mpimg.imread(image_filename)
+            imgs.append(img)
+        else:
+            print('File ' + image_filename + ' does not exist')
+
+    num_images = len(imgs)
+    IMG_WIDTH = imgs[0].shape[0]
+    IMG_HEIGHT = imgs[0].shape[1]
+    N_PATCHES_PER_IMAGE = (IMG_WIDTH / img_patch_size) * (IMG_HEIGHT / img_patch_size)
+
+    img_patches = [img_crop(imgs[i], img_patch_size, img_patch_size) for i in range(num_images)]
+    data = [img_patches[i][j] for i in range(len(img_patches)) for j in range(len(img_patches[i]))]
+    print(np.asarray(data).shape)
+    return np.asarray(data)
+
+
+# Extract label images
+def extract_labels(filename, num_images,img_patch_size):
+    """Extract the labels into a 1-hot matrix [image index, label index]."""
+    gt_imgs = []
+    for i in range(1, num_images + 1):
+        imageid = "satImage_%.3d" % i
+        image_filename = filename + imageid + ".png"
+        if os.path.isfile(image_filename):
+            print('Loading ' + image_filename)
+            img = mpimg.imread(image_filename)
+            gt_imgs.append(img)
+        else:
+            print('File ' + image_filename + ' does not exist')
+
+    num_images = len(gt_imgs)
+    gt_patches = [img_crop(gt_imgs[i], img_patch_size, img_patch_size) for i in range(num_images)]
+    data = np.asarray([gt_patches[i][j] for i in range(len(gt_patches)) for j in range(len(gt_patches[i]))])
+    labels = np.asarray([value_to_class(np.mean(data[i])) for i in range(len(data))])
+
+    # Convert to dense 1-hot representation.
+    return labels.astype(np.float32)
