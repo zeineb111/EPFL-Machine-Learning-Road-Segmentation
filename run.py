c0f1fc11c4eb87e8d6c3195a0aab21829f4c3f37
Olivier Lam <olivier.lam@epfl.ch>
2019-12-14 14:59:03 +0100

    Ready for testing, waiting for images reshaped

--- run.py
+++ run.py
@@ -2,10 +2,11 @@ import tensorflow as tf
 from utils.helpers import *
 from PIL import Image
 
-PATH = '/content/drive/My Drive/EPFL-Machine-Learning-Road-Segmentation'
+PATH = '/content/drive/My Drive/Road_Segmentation'
 PATH_MODEL = '/content/unet_model.h5'
 PATH_DATA = PATH + '/data/training/images/'
-PATH_PREDICTION_DIR = '/predictions/'
+PATH_PREDICTION_DIR = '/predictions'
+PATH_SUBMISSION = PATH + '/submission.csv'
 
 
 def main(argv=None):
@@ -16,14 +17,16 @@ def main(argv=None):
     print('Loading Data...')
     data = load_data(PATH_DATA)
 
+    images_filenames = []
     predictions = model.predict(data)
 
     for i in range(predictions.shape[0]):
         pimp = np.squeeze(predictions[i]).round()
         pimp = img_float_to_uint8(pimp)
 
-        image_filename = PATH + PATH_PREDICTION_DIR + 'pred_' + str(i + 1) + '.png'
+        image_filename = PATH + PATH_PREDICTION_DIR + '/pred_' + str(i + 1) + '.png'
         Image.fromarray(pimp).save(image_filename)
+        images_filenames.append(image_filename)
 
 
 if __name__ == '__main__':
