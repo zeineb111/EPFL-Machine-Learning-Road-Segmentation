b3a05a453d490cb2d1992e8af3d430149924d186
jalel <jalel.zghonda@epfl.ch>
2019-12-15 16:14:38 +0100

    migrated to tf.keras, first implementation

---
+++ src/CnnModel.py
@@ -0,0 +1,139 @@
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras import models, layers
+import numpy as np
+import collections
+from utils.helpers import *
+
+
+class CnnModel(keras.Model):
+
+    def __init__(self):
+        super(CnnModel, self).__init__()
+        self.patch_size = 16
+        self.window_size = 72
+        self.nb_channels = 3
+        self.leak_alpha = 0.1
+        self.dropout_prob = 0.25
+        self.regularization_value = 1e-6
+        self.nb_classes = 2
+        self.batch_size = 25
+        self.create_model()
+
+    def create_model(self):
+        self.model = keras.Sequential()
+        input_shape = (self.window_size, self.window_size, self.nb_channels)
+
+        self.model.add(layers.Convolution2D(64, 5, 5, input_shape=input_shape))
+        self.model.add(layers.LeakyReLU(alpha=self.leak_alpha))
+        self.model.add(layers.MaxPool2D())
+        self.model.add(layers.Dropout(self.dropout_prob))
+
+        self.model.add(layers.Convolution2D(128, 3, 3))
+        self.model.add(layers.LeakyReLU(alpha=self.leak_alpha))
+        self.model.add(layers.MaxPool2D())
+        self.model.add(layers.Dropout(self.dropout_prob))
+
+        self.model.add(layers.Flatten())
+        self.model.add(layers.Dense(256, kernel_regularizer=keras.regularizers.l2(self.regularization_value)))
+        self.model.add(layers.LeakyReLU(alpha=self.leak_alpha))
+        self.model.add(layers.Dropout(self.dropout_prob * 2))
+
+        self.model.add(
+            layers.Dense(self.nb_classes, kernel_regularizer=keras.regularizers.l2(self.regularization_value),
+                         activation='softmax'))
+
+    def train_model(self, gt_imgs, tr_imgs, nb_epochs=100):
+
+        def softmax_categorical_crossentropy(y_true, y_pred):
+            """
+            Uses categorical cross-entropy from logits in order to improve numerical stability.
+            This is especially useful for TensorFlow (less useful for Theano).
+            """
+            return keras.backend.categorical_crossentropy(y_pred, y_true, from_logits=True)
+
+        optimizer = keras.optimizers.Adam()
+        self.model.compile(optimizer=optimizer, loss=keras.losses.binary_crossentropy,
+                           metrics=['accuracy'])
+
+        np.random.seed(1)  # for reproducibility
+
+        def generate_minibatch():
+            """
+            Procedure for real-time minibatch creation and image augmentation.
+            This runs in a parallel thread while the model is being trained.
+            """
+            while True:
+                # Generate one minibatch
+                X_batch = np.empty((self.batch_size, self.window_size, self.window_size, 3))
+                Y_batch = np.empty((self.batch_size, 2))
+                for i in range(self.batch_size):
+                    # Select a random image
+                    idx = np.random.choice(tr_imgs.shape[0])
+                    shape = tr_imgs[idx].shape
+
+                    # Sample a random window from the image
+                    center = np.random.randint(self.window_size // 2, shape[0] - self.window_size // 2, 2)
+                    sub_image = tr_imgs[idx][center[0] - self.window_size // 2:center[0] + self.window_size // 2,
+                                center[1] - self.window_size // 2:center[1] + self.window_size // 2]
+                    gt_sub_image = gt_imgs[idx][center[0] - self.patch_size // 2:center[0] + self.patch_size // 2,
+                                   center[1] - self.patch_size // 2:center[1] + self.patch_size // 2]
+
+                    threshold = 0.25
+                    label = (np.array([np.mean(gt_sub_image)]) > threshold) * 1
+
+                    label = keras.utils.to_categorical(label, self.nb_classes)
+                    X_batch[i] = sub_image
+                    Y_batch[i] = label
+
+                yield (X_batch, Y_batch)
+
+        samples_per_epoch = 62500
+        print("samples per epoch %d" % samples_per_epoch)
+
+        # This callback reduces the learning rate when the training accuracy does not improve any more
+        lr_callback = keras.callbacks.ReduceLROnPlateau(monitor='accuracy', factor=0.5, patience=5,
+                                                        verbose=1, mode='auto', epsilon=0.0001, cooldown=0, min_lr=0)
+
+        # Stops the training process upon convergence
+        stop_callback = keras.callbacks.EarlyStopping(monitor='accuracy', min_delta=0.0001, patience=11, verbose=1,
+                                                      mode='auto')
+
+        try:
+            self.model.fit_generator(generate_minibatch(),
+                                     steps_per_epoch=samples_per_epoch,
+                                     epochs=nb_epochs,
+                                     verbose=1,
+                                     callbacks=[lr_callback, stop_callback])
+        except KeyboardInterrupt:
+            # Do not throw away the model in case the user stops the training process
+            pass
+
+        print('Training completed')
+
+    def save(self,
+             filepath,
+             overwrite=True,
+             include_optimizer=True,
+             save_format=None,
+             signatures=None,
+             options=None):
+        self.model.save_weights(filepath)
+
+    def load_weights(self, filepath, by_name=False):
+        self.load_weights(filepath)
+
+    def predict(self,
+                x,
+                batch_size=None,
+                verbose=0,
+                steps=None,
+                callbacks=None,
+                max_queue_size=10,
+                workers=1,
+                use_multiprocessing=False):
+        X_patches = gen_patches(preprocess_imgs(x, self.window_size, self.patch_size), self.window_size,
+                                self.patch_size)
+        Y_pred = self.model.predict(X_patches)
+        Y_pred = (Y_pred[:, 0] < Y_pred[:, 1]) * 1
+        return Y_pred
--- utils/helpers.py
+++ utils/helpers.py
@@ -1,87 +1,124 @@
-# Helper functions
 import os
 
-import matplotlib.image as mpimg
 import numpy as np
 
-PIXEL_DEPTH = 255
+from PIL import Image
+import tensorflow as tf
+import matplotlib.image as mpimg
 
 
-def load_image(infilename):
-    data = mpimg.imread(infilename)
-    return data
+# Write predictions from neural network to a file
+def write_predictions_to_file(predictions, labels, filename):
+    max_labels = np.argmax(labels, 1)
+    max_predictions = np.argmax(predictions, 1)
+    file = open(filename, "w")
+    n = predictions.shape[0]
+    for i in range(0, n):
+        file.write(max_labels(i) + ' ' + max_predictions(i))
+    file.close()
 
 
-def img_float_to_uint8(img):
+# Print predictions from neural network
+def print_predictions(predictions, labels):
+    max_labels = np.argmax(labels, 1)
+    max_predictions = np.argmax(predictions, 1)
+    print(str(max_labels) + ' ' + str(max_predictions))
+
+
+# Assign a label to a patch v
+def value_to_class(v):
+    foreground_threshold = 0.25  # percentage of pixels > 1 required to assign a foreground label to a patch
+    df = np.sum(v)
+    if df > foreground_threshold:  # road
+        return [0, 1]
+    else:  # bgrd
+        return [1, 0]
+
+
+# Convert array of labels to an image
+def label_to_img(imgwidth, imgheight, w, h, labels):
+    array_labels = np.zeros([imgwidth, imgheight])
+    idx = 0
+    for i in range(0, imgheight, h):
+        for j in range(0, imgwidth, w):
+            if labels[idx][0] > 0.5:  # bgrd
+                l = 0
+            else:
+                l = 1
+            array_labels[j:j + w, i:i + h] = l
+            idx = idx + 1
+    return array_labels
+
+
+def img_float_to_uint8(img, pixel_depth):
     rimg = img - np.min(img)
-    rimg = (rimg / np.max(rimg) * 255).round().astype(np.uint8)
+    rimg = (rimg / np.max(rimg) * pixel_depth).round().astype(np.uint8)
     return rimg
 
 
-# Concatenate an image and its groundtruth
-def concatenate_images(img, gt_img):
-    nChannels = len(gt_img.shape)
-    w = gt_img.shape[0]
-    h = gt_img.shape[1]
-    if nChannels == 3:
-        cimg = np.concatenate((img, gt_img), axis=1)
-    else:
-        gt_img_3c = np.zeros((w, h, 3), dtype=np.uint8)
-        gt_img8 = img_float_to_uint8(gt_img)
-        gt_img_3c[:, :, 0] = gt_img8
-        gt_img_3c[:, :, 1] = gt_img8
-        gt_img_3c[:, :, 2] = gt_img8
-        img8 = img_float_to_uint8(img)
-        cimg = np.concatenate((img8, gt_img_3c), axis=1)
-    return cimg
-
-
-def img_crop(im, w, h):
+# Extract patches from a given image
+def img_crop(im, w, h, padding):
     list_patches = []
     imgwidth = im.shape[0]
     imgheight = im.shape[1]
-    is_2d = len(im.shape) < 3
-    for i in range(0, imgheight, h):
-        for j in range(0, imgwidth, w):
-            if is_2d:
+    is_gt = len(im.shape) < 3
+    for i in range(padding, imgheight + padding, h):
+        for j in range(padding, imgwidth + padding, w):
+            if is_gt:
                 im_patch = im[j:j + w, i:i + h]
             else:
-                im_patch = im[j:j + w, i:i + h, :]
+                im_patch = im[j - padding:j + w + padding, i - padding:i + h + padding, :]
             list_patches.append(im_patch)
     return list_patches
 
 
-def img_float_to_uint8(img):
-    rimg = img - np.min(img)
-    rimg = (rimg / np.max(rimg) * PIXEL_DEPTH).round().astype(np.uint8)
-    return rimg
+def pad_image(img, padding_size):
+    """
+    Extend the canvas of an image. Mirror boundary conditions are applied.
+    """
+    if len(img.shape) < 3:
+        # Greyscale image (ground truth)
+        data = np.pad(img, ((padding_size, padding_size), (padding_size, padding_size)), 'reflect')
+    else:
+        # RGB image
+        data = np.pad(img, ((padding_size, padding_size), (padding_size, padding_size), (0, 0)), 'reflect')
+    return data
 
 
-def load_data(data_path):
-    files = os.listdir(data_path)
-    n = len(files)
+def load_image(filename):
+    if os.path.isfile(filename):
+        img = mpimg.imread(filename)
+        return img
+    else:
+        raise Exception('File ' + filename + ' does not exist')
 
-    imgs = [load_image(data_path + '/' + files[i]) for i in range(n)]
 
-    return np.asarray(imgs)
+def load_images(train_dir, gt_dir, num_images):
+    print("loading images from disk .. ")
+    imgs = np.asarray([load_image(train_dir + "satImage_%.4d" % i + ".png") for i in range(1, num_images + 1)])
+    gt_imgs = np.asarray([load_image(gt_dir + "satImage_%.4d" % i + ".png") for i in range(1, num_images + 1)])
+    print("finished loading images from disk ..")
+    print("imgs shape : ", end=' ')
+    print(imgs.shape)
+    return imgs, gt_imgs
 
 
-def img_predict(img, model):
-    width = img.shape[0]
-    height = img.shape[1]
-    img1 = img[:400, :400]
-    img2 = img[:400, -400:]
-    img3 = img[-400:, :400]
-    img4 = img[-400:, -400:]
+def gen_patches(imgs, window_size, patch_size):
+    padding_size = int((window_size - patch_size) / 2)
 
-    imgs = np.array([img1, img2, img3, img4])
-    predictions = model.predict(imgs)
+    img_patches = np.asarray(
+        [img_crop(imgs[i], patch_size, patch_size, padding_size) for i in range(imgs.shape[0])])
 
-    prediction = np.zeros((width, height, 1))
+    if len(img_patches.shape) > 3:
+        img_patches = img_patches.reshape((-1, img_patches.shape[2], img_patches.shape[3], img_patches.shape[4]))
+    else:
+        img_patches = img_patches.reshape((-1, img_patches.shape[2], img_patches.shape[3]))
 
-    prediction[:400, :400] = predictions[0]
-    prediction[:400, -400:] = predictions[1]
-    prediction[-400:, :400] = predictions[2]
-    prediction[-400:, -400:] = predictions[3]
+    return img_patches
 
-    return prediction
+
+def preprocess_imgs(imgs, window_size, patch_size):
+    padding_size = int((window_size - patch_size) / 2)
+    padded_imgs = np.asarray([pad_image(imgs[i], padding_size) for i in range(imgs.shape[0])])
+
+    return padded_imgs
